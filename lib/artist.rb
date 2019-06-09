63fcd3add29a1108d937687f65b9aaa161949f40
David Gilbert <dagilbert333@gmail.com>
2019-06-09 17:27:34 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist 
+  attr_accessor :name, :song
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song  
+  end
+end
